f56510d16b3188bec57ea9bda253f3cb17f61e71
secouba <secouba8@gmail.com>
2021-07-16 19:54:32 +0200

    urls

--- testsSharingCloud/testsSharingCloud/urls.py
+++ testsSharingCloud/testsSharingCloud/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from resourceManagement import views
 
 urlpatterns = [
+    path('', views.index),
+    re_path(r'^resourceManagement/', include('resourceManagement.urls')),
     path('admin/', admin.site.urls),
 ]
